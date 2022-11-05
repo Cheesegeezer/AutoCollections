c3f897ac0ae32de1dd0a06459137e0a10d8f0bff
Dave <daveybryce@hotmail.co.uk>
2022-11-05 09:30:33 +0000

    1.0.0.4 - All Working now as intended

--- AutoCollections.Api/RefreshRequest.cs
+++ AutoCollections.Api/RefreshRequest.cs
@@ -1,8 +1,37 @@
+using System.Linq;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Serialization;
 using MediaBrowser.Model.Services;
+using MediaBrowser.Model.Tasks;
 
-namespace AutoCollections.Api;
+namespace AutoCollections.AutoCollections.Api;
+public class AutoGroupRefreshRequest : IService 
 
-[Route("/AutoCollections/Refresh", "POST")]
-internal class RefreshRequest
 {
+    [Route("/AutoCollections/Refresh", "POST", Summary = "Run update AutoCollections scheduled task")]
+    public class ExecuteSpotlightUpdateRequest : IReturnVoid
+    {
+
+    }
+
+    private ITaskManager TaskManager { get; set; }
+    private ILibraryManager LibraryManager { get; set; }
+    private IJsonSerializer JsonSerializer { get; set; }
+    private IRequest Request { get; set; }
+    private IUserManager UserManager { get; set; }
+    private ILogger Log { get; set; }
+    public AutoGroupRefreshRequest(ITaskManager taskManager, ILibraryManager libraryManager, IJsonSerializer json, IUserManager userManager, ILogManager logManager)
+    {
+        TaskManager = taskManager;
+        LibraryManager = libraryManager;
+        JsonSerializer = json;
+        UserManager = userManager;
+        Log = logManager.GetLogger(Plugin.Instance.Name);
+    }
+
+    public async void Post(ExecuteSpotlightUpdateRequest request)
+    {
+        await TaskManager.Execute(TaskManager.ScheduledTasks.FirstOrDefault(task => task.Name == "Auto Version Grouping"), new TaskOptions()).ConfigureAwait(false);
+    }
 }
